402bef93302cdcd8b9c3398d7aefa3a14bcfce0b
Denef Florent <florent.denef@gmail.com>
2017-05-08 14:53:23 +0200

    initial commit

---
+++ app/src/main/java/com/example/florent/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.florent.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView hello = (TextView) findViewById(R.id.textHello);
+    }
+}
